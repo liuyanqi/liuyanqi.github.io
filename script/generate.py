19b454cbfb0132d391e7c555a51760a988933be8
Jasmine Liu <jasmine@0587377098.wireless.umich.net>
2016-09-30 15:02:34 -0400

    add full img

--- script/generate.py
+++ script/generate.py
@@ -4,6 +4,6 @@ log = open("text.txt", 'w')
 
 for i in range(1,51):
 
-	x = '<a href="img/journal/thumb/%d_thumb.jpg" data-gallery><img src="img/journal/thumb/%d_thumb.jpg" width="84" height="84"/></a>\n' % (i,i)
+	x = '<a href="img/journal/full/%d.jpg" data-gallery><img src="img/journal/thumb/%d_thumb.jpg" width="84" height="84"/></a>\n' % (i,i)
 
 	log.write(x)
